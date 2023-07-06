d17f66c652e9dd518be4127f873d5931abb9cef6
obasoro <obasorokunle@gmail.com>
2023-07-06 06:07:26 -0500

    update service

--- my_new_lean/index.js
+++ my_new_lean/index.js
@@ -8,6 +8,30 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// app.listen(port, () => {
+//     console.log(`Listening App
+//     on port &{port}!`);
+//});
+
+app.get('/video', (req, res) => {
+    const path = "/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            console.errpr("An error occurred");
+            res.sendStatus(500)
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+
+        fs.createReadStream(path).pipe(res);
+    });
+
+});
+
 app.listen(port, () => {
     console.log(`Listening App
     on port &{port}!`);
